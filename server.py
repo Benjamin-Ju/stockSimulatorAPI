4d4c3e8c5feee120a576e9833d2180347c6e73fc
Benjamin-Ju <benjaminyanju@gmail.com>
2019-11-16 21:44:52 -0500

    Added post api call

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from sqlalchemy import create_engine
 from json import dumps
 from flask_jsonpify import jsonify
 
 import psycopg2
+import psycopg2.extras
 import datetime as dt
 from flask_cors import CORS
 
@@ -13,7 +14,7 @@ connection = psycopg2.connect(user = "postgres",
                               host = "localhost",
                               port = "5432",
                               database = "stockSimulator")
-cursor = connection.cursor()
+cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 db_connect = create_engine('postgresql://postgres:chicken!1@localhost:5432/stockSimulator')
 conn = db_connect.connect()
@@ -21,13 +22,29 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
+parser = reqparse.RequestParser()
+parser.add_argument('username')
+parser.add_argument('fullname')
+parser.add_argument('password')
+
 class Users(Resource):
-    def get(self, date):
+    def get(self):
         cursor.execute("SELECT * FROM users")
         users = cursor.fetchall()
-        return {users}
+        return jsonify(users)
+
+# curl http://127.0.0.1:5003/users/ -d "username=test1" -d "fullname=test1" -d "password=test1" -v
+
+    def post(self):
+        args = parser.parse_args()
+        cursor.execute("""
+        INSERT INTO users (username, fullname, password)
+        VALUES (%s, %s, %s);
+        """, (args['username'], args['fullname'], args['password']))
+        connection.commit()
+
 
 api.add_resource(Users, '/users/')
 
 if __name__ == '__main__':
-    app.run(port='5002')
+    app.run(port='5003', debug=True)
